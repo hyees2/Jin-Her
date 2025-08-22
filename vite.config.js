e0b1cc2053cbc055c17af00d915bb49454e29f52
Jin <57627107+hyees2@users.noreply.github.com>
2025-08-22 02:12:23 +0200

    fix: Correct base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Jin-Her/', // <--- 이 라인을 추가하세요.
-});
+  base: './', // 이 부분을 추가하세요
+})
