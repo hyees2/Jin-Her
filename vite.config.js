05fe15f922c2b9071b003a61821db5a1050a3f97
Jin <57627107+hyees2@users.noreply.github.com>
2025-08-22 02:18:38 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Jin-Her/',   // 🔴 리포 이름과 동일하게!
   plugins: [react()],
-  base: '/Jin-Her/', // 이 부분을 수정하세요.
 })
